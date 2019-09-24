aa3da870e0c2f6df0808433dcda14c52fafcfb05
Lena <lenarayskaya00709@gmail.com>
2019-09-24 23:37:15 +0300

    Tasks 1-3, 5

---
+++ Task01/Task01/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Area;
+            Console.WriteLine("Введите значения сторон прямоугольника");
+            Console.Write("Введите значение стороны а: ");
+            string stA = Console.ReadLine();
+            int SideA;
+            if (int.TryParse(stA, out SideA) == false) { }
+            Console.Write("Введите значение стороны b: ");
+            //int SideB = int.Parse(Console.ReadLine());
+            string stB = Console.ReadLine();
+            int SideB;
+            if (int.TryParse(stB, out SideB) == false) { }
+            if ((SideA <= 0) || (SideB <= 0))
+            {
+                Console.WriteLine("Введено неверное значение!");
+            }
+            else
+            {
+                Area = SideA * SideB;
+                Console.WriteLine("Площадь треугольника S равна: " + Area);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
