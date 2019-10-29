c9f0f2c9b40b9495c42558b5de2228f27e21318f
Lena <lenarayskaya00709@gmail.com>
2019-10-29 23:42:17 +0300

    Task 9 (change 1 and 2)

--- Task10/Rayskaya_Elena_Task10/Task1/Program.cs
+++ Task10/Rayskaya_Elena_Task10/Task1/Program.cs
@@ -3,34 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace Task1
 {
     class Program
     {
-        public delegate char[] SortDelegate(char[] str);
-        public static char[] SortingAlphavit(char[] str)
+        public delegate string SortDelegate(string str);
+        public static string SortingAlphavit(string str)
         {
-            char help;
+            //string st2 = string.Join("[a-z]", str);
+            char[] result = str.ToCharArray();
+            char[] help=new char[str.Length];
             for (int i = 0; i < str.Length; i++)
             {
                 for (int j = i + 1; j < str.Length; j++)
                 {
-                    if (str[j] > str[i])
+                    if (result[j] > result[i])
                     {
-                        help = str[j];
-                        str[j] = str[i];
-                        str[i] = help;
+                        help[i] = result[j];
+                        result[j] = result[i];
+                        result[i] = help[i];
                     }
                 }
             }
-            return str;
+            string strEnd = result.ToString();
+            //string[] stringInArray = strEnd.Split(new char[] { 'a', 'b', 'c', 'd', 'e' });
+            return strEnd;
         }
         static void Main(string[] args)
         {
             Console.WriteLine("Введите первую строку: ");
             string str = Console.ReadLine();
             string[] words = str.Split(new char[] { ' ' });
+
             SortDelegate funct1 = new SortDelegate(SortingAlphavit);//создание делегата и передача функции сортировки в качестве параметра
             for (int i = 0; i < words.Length; i++)
             {
@@ -38,16 +44,17 @@ namespace Task1
                 {
                     if (words[i].Length < words[j].Length)
                     {
-                        string help;
-                        help = words[j];
+                        string[] help=new string[words.Length];
+                        help[i] = words[j];
                         words[j] = words[i];
-                        words[i] = help;
+                        words[i] = help[i];
                     }
                     if (words[i].Length == words[j].Length)
                     {
                         //вызов делегата
-                        string a = funct1(words[i].ToCharArray()).ToString();
-                        words[i] = a;
+                        //char[] b = words[i].ToCharArray();
+                        var a = funct1(words[i]);
+                        //words[i] = a.ToString();
                     }
                 }
             }
---
+++ Task10/Rayskaya_Elena_Task10/Task2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Person
+    {
+        private string name;
+        public  string Name { get; set; }
+        public void Greet()
+        {
+            Console.WriteLine(Name);
+        }
+       /* public Person()
+        {
+            Name = name;
+        }*/
+    }
+    class Program
+    {
+        delegate void Message(); // 1. Объявляем делегат
+        private static void GoodMorning()
+        {
+            Console.WriteLine("Good Morning");
+        }
+        private static void GoodEvening()
+        {
+            Console.WriteLine("Good Evening");
+        }
+        static void Main(string[] args)
+        {
+            Person john = new Person { Name = "John" };
+            Person mary = new Person { Name = "Mary" };
+            Person hugo = new Person { Name = "Hugo" };
+            Message mes; // 2. Создаем переменную делегата
+            Message mes1 = john.Greet;
+            if (DateTime.Now.Hour < 12)
+            {
+                Message greetByJohn = new Message(john.Greet);
+                Message greetByMary = new Message(mary.Greet);
+                Message greetByHugo = new Message(hugo.Greet);
+                //mes1 = john.Greet;
+                mes =mes1+  GoodMorning; // 3. Присваиваем этой переменной адрес метода
+                
+            }
+            else
+            {
+                mes = GoodEvening;
+            }
+            mes(); // 4. Вызываем метод
+            Console.ReadKey();
+        }
+        
+    }
+    
+}
