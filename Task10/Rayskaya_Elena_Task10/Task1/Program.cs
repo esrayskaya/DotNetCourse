867753521e3c9bda8b8ab29cd3dd77285ded0831
Lena <lenarayskaya00709@gmail.com>
2019-10-30 23:37:41 +0300

    Task 10(1-2)

--- Task10/Rayskaya_Elena_Task10/Task1/Program.cs
+++ Task10/Rayskaya_Elena_Task10/Task1/Program.cs
@@ -9,56 +9,74 @@ namespace Task1
 {
     class Program
     {
-        public delegate string SortDelegate(string str);
-        public static string SortingAlphavit(string str)
+        public delegate int CompareDelegate(string str1, string str2);
+        public static int Compare(string str1, string str2)
         {
-            //string st2 = string.Join("[a-z]", str);
-            char[] result = str.ToCharArray();
-            char[] help=new char[str.Length];
-            for (int i = 0; i < str.Length; i++)
+            if (str1.Length < str2.Length)
             {
-                for (int j = i + 1; j < str.Length; j++)
-                {
-                    if (result[j] > result[i])
-                    {
-                        help[i] = result[j];
-                        result[j] = result[i];
-                        result[i] = help[i];
-                    }
-                }
+                return 1;
             }
-            string strEnd = result.ToString();
-            //string[] stringInArray = strEnd.Split(new char[] { 'a', 'b', 'c', 'd', 'e' });
-            return strEnd;
+            else if (str1.Length > str2.Length)
+            {
+                return 2;
+            }
+            else
+            {
+                return 3;
+            }
+
         }
         static void Main(string[] args)
         {
             Console.WriteLine("Введите первую строку: ");
             string str = Console.ReadLine();
             string[] words = str.Split(new char[] { ' ' });
-
-            SortDelegate funct1 = new SortDelegate(SortingAlphavit);//создание делегата и передача функции сортировки в качестве параметра
+            CompareDelegate func1 = new CompareDelegate(Compare);
+            //CompareDelegate funct1 = new CompareDelegate(SortingAlphavit);//создание делегата и передача функции сортировки в качестве параметра
             for (int i = 0; i < words.Length; i++)
             {
                 for (int j = i + 1; j < words.Length; j++)
                 {
-                    if (words[i].Length < words[j].Length)
+                    int number = func1(words[i], words[j]);
+                    if (number == 1)
                     {
-                        string[] help=new string[words.Length];
+                        string[] help = new string[words.Length];
                         help[i] = words[j];
                         words[j] = words[i];
                         words[i] = help[i];
                     }
-                    if (words[i].Length == words[j].Length)
-                    {
-                        //вызов делегата
-                        //char[] b = words[i].ToCharArray();
-                        var a = funct1(words[i]);
-                        //words[i] = a.ToString();
-                    }
                 }
             }
-            for(int i = 0; i < words.Length; i++)
+            List<int> counter = new List<int>();
+            int count = 1;
+            List<string> list = new List<string>(); 
+            for(int i=0; i < words.Length-1; i++)
+            {
+                if(func1(words[i], words[i + 1]) == 2)
+                {
+                    //count++;
+                    list.Add(words[i]);
+                    list.Add(words[i + 1]);
+                    int index = 0;
+                    foreach (int element in counter)
+                    {
+                        Array.Sort(list.ToArray(), index, element);
+                        index += element;
+                    }
+                }
+                /*else if(func1(words[i], words[i + 1]) == 3)
+                {
+                    counter.Add(count);
+                    count = 1;
+                }*/
+            }
+            /*int index = 0;
+            foreach (int element in counter)
+            {
+                Array.Sort(words, index, element);
+                index += element;
+            }*/
+            for (int i = 0; i < words.Length; i++)
             {
                 Console.WriteLine(words[i]);
             }
--- Task10/Rayskaya_Elena_Task10/Task2/Program.cs
+++ Task10/Rayskaya_Elena_Task10/Task2/Program.cs
@@ -6,57 +6,101 @@ using System.Threading.Tasks;
 
 namespace Task2
 {
-    class Person
+    public class OfficeEventArgs : EventArgs
     {
-        private string name;
-        public  string Name { get; set; }
-        public void Greet()
+        public DateTime Time { get; }
+
+        public OfficeEventArgs(DateTime time)
         {
-            Console.WriteLine(Name);
+            Time = time;
         }
-       /* public Person()
-        {
-            Name = name;
-        }*/
     }
-    class Program
+    public class Office : HashSet<Person> // Офис - Список из неповторяющихся элементов
     {
-        delegate void Message(); // 1. Объявляем делегат
-        private static void GoodMorning()
+
+    }
+    public delegate void OfficeEventHandler(object sender, OfficeEventArgs args);//sender, args
+
+    
+    public class Person
+    {
+        private string Name { get; }
+        private event OfficeEventHandler Come;
+        private event EventHandler Leave;
+        private Office office;
+
+        //public  string Name { get; set; }
+
+        public delegate void ComeDelegate(Person people);
+        public event ComeDelegate PeopleCome;
+        public void Greet(Person people, DateTime time)
         {
-            Console.WriteLine("Good Morning");
-        }
-        private static void GoodEvening()
-        {
-            Console.WriteLine("Good Evening");
-        }
-        static void Main(string[] args)
-        {
-            Person john = new Person { Name = "John" };
-            Person mary = new Person { Name = "Mary" };
-            Person hugo = new Person { Name = "Hugo" };
-            Message mes; // 2. Создаем переменную делегата
-            //Message mes1 = john.Greet;
-            if (DateTime.Now.Hour < 12)
+            string hello;
+            if ((time.Hour < 10) && (time.Hour > 5))
             {
-                Message greetByJohn = new Message(john.Greet);
-                Message greetByMary = new Message(mary.Greet);
-                Message greetByHugo = new Message(hugo.Greet);
-                Message mes1 = john.Greet;
-                mes = GoodMorning; // 3. Присваиваем этой переменной адрес метода
-                Message mes2 = mes1 + mes;
+                hello = "Доброе утро";
+            }
+            else if ((time.Hour > 10) && (time.Hour < 17))
+            {
+                hello =  "Добрый день";
             }
             else
             {
-                Message greetByJohn = new Message(john.Greet);
-                Message greetByMary = new Message(mary.Greet);
-                Message greetByHugo = new Message(hugo.Greet);
-                Message mes1 = john.Greet;
-                mes = GoodEvening;
-                Message mes2 = mes1 + mes;
+                hello =  "Доброй ночи";
             }
-            mes(); // 4. Вызываем метод
-            Console.ReadKey();
+            Console.WriteLine($"'{hello}, {people.Name}' - сказал {Name}");
+        }
+        public void SayBye(Person people)
+        {
+            string bye = "До свидвния";
+            Console.WriteLine($"'{bye}, {people.Name}' - сказал {Name}");
+        }
+        public void OnCome(DateTime time)
+        {
+            foreach (var person in office)
+            {
+                Come += (sender, args) => { person.Greet(this, time); };
+            }
+            //Console.WriteLine($"{Name} пришел на работу в {time}.");
+            Console.WriteLine(Name + " пришел на работу");
+            Come?.Invoke(this, new OfficeEventArgs(time));
+            office.Add(this);
+        }
+        public void OnLeave()
+        {
+            office.Remove(this); // Сначала удалить из офиса. Оставшиеся начнут прощаться
+            foreach (var person in office)
+            {
+                Leave += (sender, args) => { person.SayBye(this); };
+            }
+            Console.WriteLine($"{Name} ушел с работы.");
+            Leave?.Invoke(this, new EventArgs());
+
+        }
+        public Person(string name, Office office)
+        {
+            Name = name;
+            this.office = office;
+        }
+    }
+    
+
+    class Program
+    {
+        delegate void Message(string name);
+        static void Main(string[] args)
+        {
+            Office office = new Office();
+            Person lena = new Person("Лена", office);
+            Person sasha = new Person("Саша", office);
+            Person vova = new Person("Вова", office);
+            lena.OnCome(DateTime.Now);
+            sasha.OnCome(DateTime.Now);
+            vova.OnCome(DateTime.Now);
+            lena.OnLeave();
+            sasha.OnLeave();
+            vova.OnLeave();
+            Console.ReadLine();
         }
         
     }
