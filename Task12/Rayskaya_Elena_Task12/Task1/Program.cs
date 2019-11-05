8fb6e686523823fe4958e5bab1bef01cb6a39e7c
Lena <lenarayskaya00709@gmail.com>
2019-11-05 23:17:50 +0300

    Add Task 11

---
+++ Task12/Rayskaya_Elena_Task12/Task1/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task1
+{
+    class Program
+    {
+        
+        public static List<int> ReadDate(string path)
+        {
+            List<int> list = new List<int>();
+            StreamReader sr = File.OpenText(path);
+            try
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    list.Add(int.Parse(line));
+                }
+                for (int i = 0; i < list.Count; i++)
+                {
+                    list[i] = list[i] * list[i];
+                }
+                return list;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+                return new List<int>();
+            }
+            finally
+            {
+                sr.Close();
+            }
+        }
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\Lena\DotNetCourse\Task12\disposable_task_file.txt";
+            ReadDate(path);
+            List<int> list = new List<int>();
+            StreamReader sr = File.OpenText(path);
+            try
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    list.Add(int.Parse(line));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                sr.Close();
+            }
+            for(int i = 0; i < list.Count; i++)
+            {
+                list[i] = list[i] * list[i];
+                int a = list[i];
+            }
+            string writePath= @"C:\Users\Lena\DotNetCourse\Task12\disposable_task_file.txt";
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(writePath, false, System.Text.Encoding.Default))
+                {
+                    for(int i = 0; i < list.Count; i++)
+                    {
+                        sw.WriteLine(list[i].ToString());
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ Task12/Rayskaya_Elena_Task12/Task2/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace Task2
+{
+    class Observation
+    {
+        private readonly string PathToCatalog;
+        private const string PathForLog = @"C:\Users\Lena\temp.xml";
+        public FileSystemWatcher fileWatcher;
+        private readonly XmlTextWriter xmlWriter;
+        private DirectoryInfo dirInfo;
+        public Observation(string pathToCatalog)
+        {
+            if (!Directory.Exists(pathToCatalog))
+            {
+                throw new DirectoryNotFoundException(pathToCatalog);
+            }
+            PathToCatalog = pathToCatalog;
+            Console.WriteLine("Наблюдение за каталогом.");
+            try
+            {
+                fileWatcher.Changed += CopyChange;
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        public void CopyChange(object sender, FileSystemEventArgs fileSystemEventArgs)
+        {
+            /*string writePath = @"C:\Users\Lena\DotNetCourse\Task12\";
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(writePath, false, System.Text.Encoding.Default))
+                {
+                    sw.WriteLine(pathToCatalog);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }*/
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Выберите режим работы\nДля режима наблюдения нажмите - 1\nДля режима отката изменений нажмите - 2");
+            if (Console.ReadKey().KeyChar == 49)
+            {
+                Console.WriteLine(" - Режим наблюдения.");
+            }
+            if (Console.ReadKey().KeyChar == 50)
+            {
+                Console.WriteLine(" - Режим отката изменений.");
+            }
+        //C: \Users\Lena\1
+            Console.ReadKey();
+        }
+    }
+}
